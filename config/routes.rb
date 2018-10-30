7a089b55051429bbb1f2b2ae11d6cb5e075537aa
RomainRec <romain.recher@gmail.com>
2018-10-30 16:18:19 +0100

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'contact/contact'
-  get 'team/team'
+
+
   get '/', to:'static#index'
+  get 'team/team'
+  get 'contact/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
