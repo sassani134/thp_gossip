f19f3f3740082e90c145f71e4b2fadc72d99a0ba
RomainRec <romain.recher@gmail.com>
2018-10-30 16:55:35 +0100

    test

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,6 @@
 class StaticController < ApplicationController
   def index
+  	@my_name_param = params[:id]
+	@gossip = Gossip.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require'faker'
+
+20.times do
+  gossip = Gossip.create!(
+  	author: Faker::HarryPotter.character, 
+  	content: Faker::Hipster.paragraph,
+  	)
+end
